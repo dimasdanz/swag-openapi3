4aae6c28dc5259c0f186b7e70044f1409383da38
Dimas Rullyandanu <dimasdanz@gmail.com>
2024-01-31 17:32:39 +0700

    upgrade dependency and port swag changes

--- go.mod
+++ go.mod
@@ -3,28 +3,26 @@ module github.com/dimasdanz/swag-openapi3
 go 1.20
 
 require (
-	github.com/getkin/kin-openapi v0.116.0
-	github.com/swaggo/swag v1.8.12
-	github.com/urfave/cli/v2 v2.25.3
+	github.com/getkin/kin-openapi v0.123.0
+	github.com/swaggo/swag v1.16.2
+	github.com/urfave/cli/v2 v2.27.1
 )
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
-	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
-	github.com/ghodss/yaml v1.0.0 // indirect
-	github.com/go-openapi/jsonpointer v0.19.6 // indirect
-	github.com/go-openapi/jsonreference v0.20.2 // indirect
-	github.com/go-openapi/spec v0.20.9 // indirect
-	github.com/go-openapi/swag v0.22.3 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.3 // indirect
+	github.com/go-openapi/jsonpointer v0.20.2 // indirect
+	github.com/go-openapi/jsonreference v0.20.4 // indirect
+	github.com/go-openapi/spec v0.20.14 // indirect
+	github.com/go-openapi/swag v0.22.9 // indirect
 	github.com/invopop/yaml v0.2.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
-	github.com/perimeterx/marshmallow v1.1.4 // indirect
+	github.com/perimeterx/marshmallow v1.1.5 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/tools v0.9.1 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20231213231151-1d8dd44e695e // indirect
+	golang.org/x/tools v0.17.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	sigs.k8s.io/yaml v1.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/getkin/kin-openapi/openapi2"
 	"github.com/getkin/kin-openapi/openapi2conv"
@@ -21,23 +22,31 @@ import (
 // https://github.com/swaggo/swag/blob/master/cmd/swag/main.go
 
 const (
-	searchDirFlag         = "dir"
-	excludeFlag           = "exclude"
-	generalInfoFlag       = "generalInfo"
-	propertyStrategyFlag  = "propertyStrategy"
-	outputFlag            = "output"
-	parseVendorFlag       = "parseVendor"
-	parseDependencyFlag   = "parseDependency"
-	markdownFilesFlag     = "markdownFiles"
-	codeExampleFilesFlag  = "codeExampleFiles"
-	parseInternalFlag     = "parseInternal"
-	generatedTimeFlag     = "generatedTime"
-	requiredByDefaultFlag = "requiredByDefault"
-	parseDepthFlag        = "parseDepth"
-	instanceNameFlag      = "instanceName"
-	overridesFileFlag     = "overridesFile"
-	parseGoListFlag       = "parseGoList"
-	quietFlag             = "quiet"
+	searchDirFlag            = "dir"
+	excludeFlag              = "exclude"
+	generalInfoFlag          = "generalInfo"
+	propertyStrategyFlag     = "propertyStrategy"
+	outputFlag               = "output"
+	outputTypesFlag          = "outputTypes"
+	parseVendorFlag          = "parseVendor"
+	parseDependencyFlag      = "parseDependency"
+	parseDependencyLevelFlag = "parseDependencyLevel"
+	markdownFilesFlag        = "markdownFiles"
+	codeExampleFilesFlag     = "codeExampleFiles"
+	parseInternalFlag        = "parseInternal"
+	generatedTimeFlag        = "generatedTime"
+	requiredByDefaultFlag    = "requiredByDefault"
+	parseDepthFlag           = "parseDepth"
+	instanceNameFlag         = "instanceName"
+	overridesFileFlag        = "overridesFile"
+	parseGoListFlag          = "parseGoList"
+	quietFlag                = "quiet"
+	tagsFlag                 = "tags"
+	parseExtensionFlag       = "parseExtension"
+	templateDelimsFlag       = "templateDelims"
+	packageName              = "packageName"
+	collectionFormatFlag     = "collectionFormat"
+	packagePrefixFlag        = "packagePrefix"
 )
 
 var initFlags = []cli.Flag{
@@ -77,6 +86,11 @@ var initFlags = []cli.Flag{
 	&cli.BoolFlag{
 		Name:  parseVendorFlag,
 		Usage: "Parse go files in 'vendor' folder, disabled by default",
+	},
+	&cli.IntFlag{
+		Name:    parseDependencyLevelFlag,
+		Aliases: []string{"pdl"},
+		Usage:   "Parse go files inside dependency folder, 0 disabled, 1 only parse models, 2 only parse operations, 3 parse all",
 	},
 	&cli.BoolFlag{
 		Name:    parseDependencyFlag,
@@ -127,6 +141,39 @@ var initFlags = []cli.Flag{
 		Value: true,
 		Usage: "Parse dependency via 'go list'",
 	},
+	&cli.StringFlag{
+		Name:  parseExtensionFlag,
+		Value: "",
+		Usage: "Parse only those operations that match given extension",
+	},
+	&cli.StringFlag{
+		Name:    tagsFlag,
+		Aliases: []string{"t"},
+		Value:   "",
+		Usage:   "A comma-separated list of tags to filter the APIs for which the documentation is generated.Special case if the tag is prefixed with the '!' character then the APIs with that tag will be excluded",
+	},
+	&cli.StringFlag{
+		Name:    templateDelimsFlag,
+		Aliases: []string{"td"},
+		Value:   "",
+		Usage:   "Provide custom delimeters for Go template generation. The format is leftDelim,rightDelim. For example: \"[[,]]\"",
+	},
+	&cli.StringFlag{
+		Name:  packageName,
+		Value: "",
+		Usage: "A package name of docs.go, using output directory name by default (check `--output` option)",
+	},
+	&cli.StringFlag{
+		Name:    collectionFormatFlag,
+		Aliases: []string{"cf"},
+		Value:   "csv",
+		Usage:   "Set default collection format",
+	},
+	&cli.StringFlag{
+		Name:  packagePrefixFlag,
+		Value: "",
+		Usage: "Parse only packages whose import path match the given prefix, comma separated",
+	},
 }
 
 func initAction(ctx *cli.Context) error {
@@ -138,21 +185,50 @@ func initAction(ctx *cli.Context) error {
 		return fmt.Errorf("not supported %s propertyStrategy", strategy)
 	}
 
+	leftDelim, rightDelim := "{{", "}}"
+
+	if ctx.IsSet(templateDelimsFlag) {
+		delims := strings.Split(ctx.String(templateDelimsFlag), ",")
+		if len(delims) != 2 {
+			return fmt.Errorf("exactly two template delimeters must be provided, comma separated")
+		} else if delims[0] == delims[1] {
+			return fmt.Errorf("template delimiters must be different")
+		}
+		leftDelim, rightDelim = strings.TrimSpace(delims[0]), strings.TrimSpace(delims[1])
+	}
+
+	outputTypes := strings.Split(ctx.String(outputTypesFlag), ",")
+	if len(outputTypes) == 0 {
+		return fmt.Errorf("no output types specified")
+	}
 	logger := log.New(os.Stdout, "", log.LstdFlags)
 	if ctx.Bool(quietFlag) {
 		logger = log.New(io.Discard, "", log.LstdFlags)
+	}
+
+	collectionFormat := swag.TransToValidCollectionFormat(ctx.String(collectionFormatFlag))
+	if collectionFormat == "" {
+		return fmt.Errorf("not supported %s collectionFormat", ctx.String(collectionFormat))
+	}
+
+	var pdv = ctx.Int(parseDependencyLevelFlag)
+	if pdv == 0 {
+		if ctx.Bool(parseDependencyFlag) {
+			pdv = 1
+		}
 	}
 
 	outputDir := ctx.String(outputFlag)
 	err := gen.New().Build(&gen.Config{
 		SearchDir:           ctx.String(searchDirFlag),
 		Excludes:            ctx.String(excludeFlag),
+		ParseExtension:      ctx.String(parseExtensionFlag),
 		MainAPIFile:         ctx.String(generalInfoFlag),
 		PropNamingStrategy:  strategy,
 		OutputDir:           outputDir,
 		OutputTypes:         []string{"json"},
 		ParseVendor:         ctx.Bool(parseVendorFlag),
-		ParseDependency:     ctx.Bool(parseDependencyFlag),
+		ParseDependency:     pdv,
 		MarkdownFilesDir:    ctx.String(markdownFilesFlag),
 		ParseInternal:       ctx.Bool(parseInternalFlag),
 		GeneratedTime:       ctx.Bool(generatedTimeFlag),
@@ -162,7 +238,13 @@ func initAction(ctx *cli.Context) error {
 		InstanceName:        ctx.String(instanceNameFlag),
 		OverridesFile:       ctx.String(overridesFileFlag),
 		ParseGoList:         ctx.Bool(parseGoListFlag),
+		Tags:                ctx.String(tagsFlag),
+		LeftTemplateDelim:   leftDelim,
+		RightTemplateDelim:  rightDelim,
+		PackageName:         ctx.String(packageName),
 		Debugger:            logger,
+		CollectionFormat:    collectionFormat,
+		PackagePrefix:       ctx.String(packagePrefixFlag),
 	})
 	if err != nil {
 		return err
